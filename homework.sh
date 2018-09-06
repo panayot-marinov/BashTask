6b087a4245f79130d424f52dbf1d880e79fc9518
Panayot Marinov <panayot.marinov12@gmail.com>
2018-09-07 01:59:53 +0300

    Added more features

--- homework.sh
+++ homework.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-PARAM1=$1
-PARAM2=$2
-PARAM3=$3
-PARAM4=$4
-PARAM5=$5
-PARAM6=$6
-PARAM7=$7
-PARAM8=$8
-PARAM9=$9
+VARIABLES_FILE=$1;
 
-cp text.txt /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM1/g" /tmp/text.txt
-sed -ie -r "s/@PARAM2@/$PARAM2/g" /tmp/text.txt
-sed -ie -r "s/@PARAM3@/$PARAM3/g" /tmp/text.txt
-sed -ie -r "s/@PARAM4@/$PARAM4/g" /tmp/text.txt
-sed -ie -r "s/@PARAM5@/$PARAM5/g" /tmp/text.txt
-sed -ie -r "s/@PARAM6@/$PARAM6/g" /tmp/text.txt
-sed -ie -r "s/@PARAM7@/$PARAM7/g" /tmp/text.txt
-sed -ie -r "s/@PARAM8@/$PARAM8/g" /tmp/text.txt
-sed -ie -r "s/@PARAM9@/$PARAM9/g" /tmp/text.txt
-cat /tmp/text.txt
+cp text.txt /tmp/text.txt;
+echo $VARIABLES_FILE;
+while read -r line
+do
+    s=$line;
+    name="$(cut -d'=' -f1 <<<"$s")";
+    value="$(cut -d'=' -f2 <<<"$s")"
+   
+    echo $name;
+    echo $value;
+    
+    sed -ie -r "s/@$name@/$value/g" /tmp/text.txt;
+
+done < "$VARIABLES_FILE"
+
+cat /tmp/text.txt;
 
 
