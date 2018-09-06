0d5a72c8aff4c60d165f8f6e11803f3e5af7a2d8
Panayot Marinov <panayot.marinov12@gmail.com>
2018-09-07 00:12:42 +0300

    One more fix

--- homework.sh
+++ homework.sh
@@ -11,14 +11,14 @@ PARAM9=$9
 
 cp text.txt /tmp/text.txt
 sed -ie -r "s/@PARAM1@/$PARAM1/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM2/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM3/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM4/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM5/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM6/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM7/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM8/g" /tmp/text.txt
-sed -ie -r "s/@PARAM1@/$PARAM9/g" /tmp/text.txt
+sed -ie -r "s/@PARAM2@/$PARAM2/g" /tmp/text.txt
+sed -ie -r "s/@PARAM3@/$PARAM3/g" /tmp/text.txt
+sed -ie -r "s/@PARAM4@/$PARAM4/g" /tmp/text.txt
+sed -ie -r "s/@PARAM5@/$PARAM5/g" /tmp/text.txt
+sed -ie -r "s/@PARAM6@/$PARAM6/g" /tmp/text.txt
+sed -ie -r "s/@PARAM7@/$PARAM7/g" /tmp/text.txt
+sed -ie -r "s/@PARAM8@/$PARAM8/g" /tmp/text.txt
+sed -ie -r "s/@PARAM9@/$PARAM9/g" /tmp/text.txt
 cat /tmp/text.txt
 
 
